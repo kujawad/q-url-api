055bf7e39c4f1865a8a8ba8f2f9e6f14b39ee62b
kujawad <kujawa.dp@gmail.com>
2022-09-01 18:51:23 +0000

    Use if in workflow for deploy and docker

--- src/main/java/com/qurlapi/qurlapi/controller/QUrlController.java
+++ src/main/java/com/qurlapi/qurlapi/controller/QUrlController.java
@@ -6,7 +6,7 @@ import com.qurlapi.qurlapi.dto.request.QUrlRequest;
 import com.qurlapi.qurlapi.model.QUrl;
 import com.qurlapi.qurlapi.service.QUrlService;
 import com.qurlapi.qurlapi.validation.StampExists;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.validation.annotation.Validated;
@@ -17,13 +17,10 @@ import javax.validation.Valid;
 @Validated
 @RestController
 @RequestMapping(path = {"/api"})
+@AllArgsConstructor
 public class QUrlController implements QUrlControllerApiInfo {
-    private final QUrlService qUrlService;
 
-    @Autowired
-    public QUrlController(final QUrlService qUrlService) {
-        this.qUrlService = qUrlService;
-    }
+    private final QUrlService qUrlService;
 
     @GetQUrlsInfo
     @CrossOrigin
