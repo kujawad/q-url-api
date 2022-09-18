a66cae3966891fc55d912c417830762fb49bec5c
kujawad <kujawa.dp@gmail.com>
2022-09-18 18:52:43 +0000

    Add liquibase after docker deploy and before Render.com hook

--- src/main/java/com/qurlapi/qurlapi/config/NativeConfiguration.java
+++ src/main/java/com/qurlapi/qurlapi/config/NativeConfiguration.java
@@ -5,13 +5,15 @@ import com.qurlapi.qurlapi.dto.response.ProblemResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.SynthesizedAnnotation;
+import org.springframework.nativex.hint.JdkProxyHint;
 import org.springframework.nativex.hint.NativeHint;
 import org.springframework.nativex.hint.TypeAccess;
 import org.springframework.nativex.hint.TypeHint;
 
-@NativeHint(types = {
-        @TypeHint(types = PropertyNamingStrategies.KebabCaseStrategy.class, access = TypeAccess.PUBLIC_CONSTRUCTORS),
-        @TypeHint(types = {Tag.class, SynthesizedAnnotation.class, ProblemResponse.class})})
+@NativeHint(jdkProxies = {@JdkProxyHint(types = {Tag.class, SynthesizedAnnotation.class})},
+            types = {@TypeHint(types = {ProblemResponse.class}),
+                     @TypeHint(types = PropertyNamingStrategies.KebabCaseStrategy.class,
+                               access = TypeAccess.PUBLIC_CONSTRUCTORS)})
 @Configuration
 public class NativeConfiguration {
 }
