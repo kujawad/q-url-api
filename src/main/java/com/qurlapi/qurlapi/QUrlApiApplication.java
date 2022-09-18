8e823f96a51b12666a11d962ceb900d57aeb01b5
kujawad <kujawa.dp@gmail.com>
2022-09-18 21:59:03 +0000

    Fix jdk proxy for native image

--- src/main/java/com/qurlapi/qurlapi/QUrlApiApplication.java
+++ src/main/java/com/qurlapi/qurlapi/QUrlApiApplication.java
@@ -1,10 +1,14 @@
 package com.qurlapi.qurlapi;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.core.annotation.SynthesizedAnnotation;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.nativex.hint.JdkProxyHint;
 
+@JdkProxyHint(types = {Tag.class, SynthesizedAnnotation.class})
 @EntityScan(basePackages = {"com.qurlapi.qurlapi.model"})
 @EnableJpaRepositories(basePackages = {"com.qurlapi.qurlapi.dao"})
 @SpringBootApplication(scanBasePackages = {"com.qurlapi.qurlapi"})
--- src/main/java/com/qurlapi/qurlapi/config/NativeConfiguration.java
+++ src/main/java/com/qurlapi/qurlapi/config/NativeConfiguration.java
@@ -2,16 +2,12 @@ package com.qurlapi.qurlapi.config;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.qurlapi.qurlapi.dto.response.ProblemResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.SynthesizedAnnotation;
-import org.springframework.nativex.hint.JdkProxyHint;
 import org.springframework.nativex.hint.NativeHint;
 import org.springframework.nativex.hint.TypeAccess;
 import org.springframework.nativex.hint.TypeHint;
 
-@NativeHint(jdkProxies = {@JdkProxyHint(types = {Tag.class, SynthesizedAnnotation.class})},
-            types = {@TypeHint(types = {ProblemResponse.class}),
+@NativeHint(types = {@TypeHint(types = {ProblemResponse.class}),
                      @TypeHint(types = PropertyNamingStrategies.KebabCaseStrategy.class,
                                access = TypeAccess.PUBLIC_CONSTRUCTORS)})
 @Configuration
